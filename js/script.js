207b5964767db0309899f74dc6440bef7397b5db
palascinochristian <palascinochristian@gmail.com>
2025-01-08 14:01:05 +0100

    add dom manipulation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+// DOM ELEMENTS
+
+const mailistElm = document.getElementById("mailist");
+
+
+for(let i=0; i<10; i++){
 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-  .then(function (response) {
-    // handle success
-    console.log(response.data.response);
+.then(function (response) {
+    let mailist = response.data.response;
+    mailistElm.innerHTML += `<li>${mailist}</li>`
   })
+}
